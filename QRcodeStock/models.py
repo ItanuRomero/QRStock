f512e3d3185f055ee9c61530a58407920ffa44f2
ItanuRomero <66224956+ItanuRomero@users.noreply.github.com>
2020-12-23 19:59:29 -0300

    Update models.py

--- QRcodeStock/models.py
+++ QRcodeStock/models.py
@@ -1,3 +1,29 @@
 from django.db import models
-
+from django.utils.translation import gettext_lazy as _
 # Create your models here.
+
+class Usuario(models.Model):
+    class TipoDeUsuario(models.TextChoices):
+        OPERADOR = 'OP', _('Operador')
+        GERENTE = 'GER', _('Gerente')
+
+    nome = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    telefone = models.CharField(max_length=20)
+    data_nascimento = models.DateField(blank=True, null=True)
+    cpf = models.CharField(max_length=15)
+    tipo = models.CharField(
+        max_length=2,
+        choices=TipoDeUsuario.choices,
+        default=TipoDeUsuario.OPERADOR,
+    )
+    senha = models.CharField(max_length=20)
+
+"""
+class Estante(models.Model):
+
+class Categoria(models.Model):
+
+class Produto(models.Model):
+
+class Remessa(models.Model):"""
